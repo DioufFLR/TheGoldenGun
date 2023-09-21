0ad15017c54478af2e9fecdec8b374b6ca7872a1
diouf <geoffreyfleur@gmail.com>
2023-09-21 14:31:02 +0200

    :hammer: Modification système

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,15 +2,47 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Supplier;
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Faker\Factory as Faker;
 
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $faker = Faker::create('fr_FR');
+
+        // Création de 10 fournisseurs
+
+        for ($i = 0; $i < 10; ++$i) {
+            $supplier = new Supplier();
+            $supplier->setSupplierName($faker->company)
+                ->setSupplierPhone($faker->phoneNumber)
+                ->setSupplierCity($faker->city)
+                ->setSupplierAdress($faker->address)
+                ->setSupplierPC($faker->postcode)
+                ->setSupplierCountry($faker->country);
+            $manager->persist($supplier);
+        }
+
+        // Création 10 users
+
+        for ($i = 0; $i < 10; ++$i) {
+            $user = new User();
+            $user->setEmail($faker->email)
+                ->setRoles($faker->randomElement(['ROLE_USER', 'ROLE_OWNER', 'ROLE_SUPER_USER']))
+                ->setPassword($faker->password)
+                ->setUserName($faker->userName)
+                ->setUserFirstName($faker->firstName)
+                ->setUserAdress($faker->address)
+                ->setUserCity($faker->city)
+                ->setUserPC($faker->postcode)
+                ->setUserPhone($faker->phoneNumber)
+                ->setUserPicture($faker->image(width: 100, height: 100))
+                ->setUserType($faker->randomElement(['0', '1', '2']));
+        }
 
         $manager->flush();
     }
