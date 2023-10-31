61108a9dcadf449aa96c50c39d1b57522ba98773
diouf <geoffreyfleur@gmail.com>
2023-10-31 11:13:06 +0100

    :hammer: index.html.twig main modif

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -7,15 +7,15 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class MainController extends AbstractController
+class CategoryController extends AbstractController
 {
-    #[Route('/', name: 'app_main')]
+    #[Route('/category', name: 'app_category')]
     public function index(CategoryRepository $categoryRepository): Response
     {
-        return $this->render('main/index.html.twig', [
-            'controller_name' => 'MainController',
-            'categories' => $categoryRepository->findById([1, 2, 3],
-                ['id' => 'asc'])
+        return $this->render('category/index.html.twig', [
+            'controller_name' => 'CategoryController',
+            'categories' => $categoryRepository->findBy([],
+            ['id' => 'asc'])
         ]);
     }
 }
